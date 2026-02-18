4dd5e8ce0c5b00bfc822406a492715a5d66ba176
SharmithaV <145974496+sharmitha3@users.noreply.github.com>
2026-02-18 21:57:03 +0530

    Update Admin.java

--- Vehicle_RentalMS/src/Admin.java
+++ Vehicle_RentalMS/src/Admin.java
@@ -1,23 +1,3 @@
-public class Admin {
-    private int admin_id;
-    private String mail;
 
-    public Admin(int admin_id, String mail) {
-        this.admin_id = admin_id;
-        this.mail = mail;
-    }
 
-    public int getAdmin_id() {
-        return admin_id;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void displayAdmin() {
-        System.out.println("Admin ID: " + admin_id);
-        System.out.println("Email: " + mail);
-    }
-}
 
